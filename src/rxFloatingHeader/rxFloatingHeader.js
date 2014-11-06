21fde04b237905482d9cfac5c448270ceeb6806a
Jay Parlar <parlar@gmail.com>
2014-11-06 17:13:28 -0500

    first go

--- src/rxFloatingHeader/rxFloatingHeader.js
+++ src/rxFloatingHeader/rxFloatingHeader.js
@@ -2,26 +2,36 @@ angular.module('encore.ui.rxFloatingHeader', [])
 .directive('rxFloatingArea', function ($window) {
     return {
         restrict: 'A',
-        link: function (scope, element, attrs) {
-            var updateHeaders = function () {
+        link: function (scope, element) {
+            scope.updateHeaders = function () {
                 var offset = element.getBoundingClientRect(),
-                    scrollTop = $window.scrollTop,
-                    
-                    
+                    scrollTop = $window.scrollTop;
+                              
+                if ((scrollTop > offset.top) && (scrollTop < offset.top + element.height())) {
+                    scope.floatingHeader.css({
+                        'visibility': 'visible'
+                    });
+                } else {
+                    scope.floatingHeader.css({
+                        'visibility': 'hidden'
+                    });
+                }
 
             };
-            $window.scroll(updateHeaders);
-            $window.trigger("scroll");
 
         },
-        controller: function ($scope) {
+        controller: function ($scope, $window) {
             this.registerHeader = function (headerRow) {
-                var headerClone = headerRow.clone();
+                $scope.floatingHeader = headerRow;
+                $scope.headerClone = headerRow.clone();
                 // jquery lite doesn't have .before(), so we use this
                 // http://stackoverflow.com/questions/21788314/angularjs-implement-elements-before-method-without-jquery
-                headerRow.parent()[0].insertBefore(headerClone[0], headerRow[0]);
-                headerRow.css("width", headerRow[0].width);
-                headerRow.addClass("rx-floating-header");
+                headerRow.parent()[0].insertBefore($scope.headerClone[0], headerRow[0]);
+                headerRow.css('width', headerRow[0].width);
+                headerRow.addClass('rx-floating-header');
+
+                $window.scroll($scope.updateHeaders);
+                $window.trigger('scroll');
             };
         }
     };
@@ -30,7 +40,7 @@ angular.module('encore.ui.rxFloatingHeader', [])
 .directive('rxFloatingHeader', function () {
     return {
         restrict: 'A',
-        requires: 'rxFloatingArea',
+        require: 'rxFloatingArea',
         link: function (scope, element, attrs, floatingAreaCtrl) {
             floatingAreaCtrl.registerHeader(element);
         }
